b641b693c1a4b47544f6ca24dea398d61fb4f05b
Fredrik Nilsson <ftnilsson@gmail.com>
2023-09-06 19:19:03 +0200

    created new project

---
+++ src/generator.ts
@@ -0,0 +1,43 @@
+﻿import Options from "./options";
+import nouns from './words/nouns';
+import lastWords from "./words/last";
+
+export default class ReleaseNameGenerator {
+
+    defaultOptions: Options = new Options('-', 2, false, false);
+
+    options: Options;
+
+    constructor(options?: Options) {
+        this.options = options ?? this.defaultOptions;
+    }
+
+    getRandomInt = (max: number): number => {
+        return Math.floor(Math.random() * max);
+    }
+
+    generate = (): string => {
+        let generated: string[] = []
+        const addString = (word: string) => {
+            let w = word.toLowerCase().replace(' ', '_');
+            if (this.options.capitalize)
+                w = word.charAt(0).toUpperCase() + word.slice(1);
+                
+            generated.push(`${w} `);
+        }
+
+        for (let i = 0; i < this.options.length - 1; i++) {
+            addString(nouns[this.getRandomInt(nouns.length)]);
+        }
+        addString(lastWords[this.getRandomInt(lastWords.length)]);
+
+        const generateToken = (): string => {
+            return Math.random().toString(36).substring(2);
+        };
+
+        if (this.options.useToken)
+            generated.push(generateToken());
+
+        return generated.join(this.options.delimiter);
+    }
+}
---
+++ src/options.ts
@@ -0,0 +1,13 @@
+﻿export default class Options {
+    delimiter?: string;
+    length: number;
+    useToken?: boolean;
+    capitalize?: boolean;
+
+    constructor(delimiter: string = '', length: number = 2, useToken: boolean = false, capitalize: boolean = false) {
+        this.delimiter = delimiter;
+        this.useToken = useToken;
+        this.length = length;
+        this.capitalize = capitalize;
+    }
+}
