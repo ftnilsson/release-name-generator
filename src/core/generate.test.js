f0bfedfe6d72781121c3ee42ba8a35b59ae1e522
Fredrik Nilsson <ftnilsson@gmail.com>
2025-03-04 08:29:33 +0100

    Update test scripts to compile before running tests and enhance test coverage for generate function

--- src/core/generate.test.js
+++ src/core/generate.test.js
@@ -1,7 +1,6 @@
-﻿const {generate} = require("../../dist/lib/es5");
+﻿const {generate} = require("../../dist/lib/es5/core/generate");
 
 describe("Generate release name with defaults", () => {
-    
     const result = generate();
     test("should give a release name", () => {       
         expect(result.length).toBeGreaterThan(0);
@@ -41,13 +40,42 @@ describe("Generate release name function with delimiter='|' and length 3 and use
     });
 
     test("words should be capitalized", () => {       
-        
         const split = result.split('|');
-        for (let i = 0; i < split.length -1; i++){
+        for (let i = 0; i < split.length - 1; i++){
             const word = split[i];            
             expect(isUppercase(word[0])).toBeTruthy();                
         }        
     });    
+});
+
+describe("Generate release name with strings", () => {
+    const delimiter = '|';
+    const length = '2';
+    const useToken = 'false';
+    const capitalize = 'false';
+
+    const result = generate(delimiter, parseInt(length), useToken === 'true', capitalize === 'true');
+    test("should give a release name", () => {       
+        expect(result.length).toBeGreaterThan(0);
+    });
+
+    test("should have a delimiter", () => {        
+        expect(result).toContain('|');
+    });
+
+    test("should consist of 2 words", () => {        
+        expect(result.split('|').length).toEqual(2);
+    });
+
+    test("words should not be capitalized", () => {       
+        const split = result.split('|');
+        for (let i = 0; i < split.length; i++){
+            const word = split[i];            
+            expect(!isUppercase(word[0])).toBeTruthy();                
+        }        
+    });  
+
+    console.log(result);
 });
 
 function isUppercase(char) {
